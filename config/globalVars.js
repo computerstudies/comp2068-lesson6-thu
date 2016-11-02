58fa56a9fa0fe62e481656dd0932c8a84efb3b10
ifotn <rich@infrontofthenet.com>
2016-11-01 20:32:23 -0400

    Delete globalVars.js

--- config/globalVars.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by RFreeman on 10/6/2016.
- */
-
-// global variables for the application
-module.exports = {
-  //db: 'mongodb://localhost/comp2068-thu'
-  db: 'mongodb://gcrfreeman2:iwillnotbelate2016@ds048319.mlab.com:48319/comp2068-thu'
-};
